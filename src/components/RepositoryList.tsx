03fe070b0fdae0380e750db25485173d398b3f19
Adriano Novais Queiroz <adriano.nq@gmail.com>
2021-05-24 21:25:08 -0300

    First version

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
+
+//https://api.github.com/users/adrianoqueiroz/repos
+
+const repository = {
+    name: 'unform',
+    description: 'Forms in React',
+    link: 'https://www.github.com/unform/unform'
+}
+
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
+export function RepositoryList() {
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/adrianoqueiroz/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []);
+
+    console.log(repositories);
+
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
+            </ul>
+        </section>
+    );
+}
